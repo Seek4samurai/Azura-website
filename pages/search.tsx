b4e253b02df9e84748d8050010427464402bfaeb
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-02-21 17:27:54 +0530

    style: 💄 changes in positioning type

--- pages/search.tsx
+++ pages/search.tsx
@@ -13,19 +13,21 @@ const style = {
   logo: `flex flex-row items-center`,
   logoImage: ``,
   meetDev: ``,
-  centerContainer: `absolute top-[15%] left-[50%] -translate-x-[50%] `,
+
+  wholeCenter: `relative flex flex-col items-center top-[10rem]`,
+  centerContainer: ``,
   searchTop: `text-white text-md`,
   searchArea: `w-[55vw] h-[5rem] rounded-xl opacity-70 pl-[1.5rem] pr-[1.5rem] outline-none focus:border-0 focus:opacity-90 placeholder:text-black`,
-  poweredContainer: `absolute left-[50%] -translate-x-[50%] flex flex-row items-center mt-[2rem]`,
+  poweredContainer: `flex flex-row justify-center mt-[2rem]`,
   poweredText: `text-[#d3d3d3]`,
   poweredImg: ``,
-  answerSection: `absolute left-[50%] -translate-x-[50%] top-[40%] text-white text-4xl text-center`,
+  answerSection: `text-white text-4xl text-center`,
   textHolder: `flex flex-row justify-center align-center items-center`,
   answer: `select-text`,
+  bottomContainer: `flex flex-col justify-center items-center`,
   emotes: `cursor-pointer text-white text-[3.5rem] flex justify-center pt-[0.5rem] pb-[0.6rem] mt-[3rem] mb-[2rem] animate-bounce`,
-  submitBtn: `absolute left-[50%] -translate-x-[50%] cursor-pointer bg-white w-[7rem] h-[2.4rem] flex justify-center items-center rounded text-black hover:bg-black hover:text-white`,
+  submitBtn: `cursor-pointer bg-white w-[7rem] h-[2.4rem] flex justify-center items-center rounded text-black hover:bg-black hover:text-white`,
   btnText: `text-xl`,
-  bottomContainer: `absolute bottom-[30%] left-[50%] -translate-x-[50%]`,
 };
 
 const Tool = () => {
@@ -119,46 +121,48 @@ const Tool = () => {
               }
             `}</style>
           </div>
-          <div className={style.centerContainer}>
-            <p className={style.searchTop}>SEARCH BY IMAGE URL</p>
-            <input
-              value={imageUrl}
-              onChange={onImgUrlChange}
-              type="url"
-              placeholder="PASTE HERE"
-              className={style.searchArea}
-            />
-            <div className={style.poweredContainer}>
-              <div className={style.poweredText}>
-                Powered by Microsoft Azure
+          <div className={style.wholeCenter}>
+            <div className={style.centerContainer}>
+              <p className={style.searchTop}>SEARCH BY IMAGE URL</p>
+              <input
+                value={imageUrl}
+                onChange={onImgUrlChange}
+                type="url"
+                placeholder="PASTE HERE"
+                className={style.searchArea}
+              />
+              <div className={style.poweredContainer}>
+                <div className={style.poweredText}>
+                  Powered by Microsoft Azure
+                </div>
+                <div className={style.poweredImg}>
+                  <a href="https://azure.microsoft.com/" target="_blank">
+                    <Image src={AzureLogo} alt="Azure" width={39} height={39} />
+                  </a>
+                </div>
               </div>
-              <div className={style.poweredImg}>
-                <a href="https://azure.microsoft.com/" target="_blank">
-                  <Image src={AzureLogo} alt="Azure" width={39} height={39} />
-                </a>
+            </div>
+            <div className={style.answerSection}>
+              <div className={style.textHolder}>
+                <span>“</span>
+                <div className={style.answer}>{description}</div>
+                <span>”</span>
               </div>
+              <style jsx>{`
+                span {
+                  font-family: sans !important;
+                  font-weight: bold;
+                  font-size: 4rem;
+                }
+              `}</style>
             </div>
-          </div>
-          <div className={style.answerSection}>
-            <div className={style.textHolder}>
-              <span>“</span>
-              <div className={style.answer}>{description}</div>
-              <span>”</span>
-            </div>
-            <style jsx>{`
-              span {
-                font-family: sans !important;
-                font-weight: bold;
-                font-size: 4rem;
-              }
-            `}</style>
-          </div>
-          <div className={style.bottomContainer}>
-            <div onClick={onTweak} className={style.emotes}>
-              {emoji}
-            </div>
-            <div onClick={onSubmit} className={style.submitBtn}>
-              <div className={style.btnText}>Search</div>
+            <div className={style.bottomContainer}>
+              <div onClick={onTweak} className={style.emotes}>
+                {emoji}
+              </div>
+              <div onClick={onSubmit} className={style.submitBtn}>
+                <div className={style.btnText}>Search</div>
+              </div>
             </div>
           </div>
         </div>
